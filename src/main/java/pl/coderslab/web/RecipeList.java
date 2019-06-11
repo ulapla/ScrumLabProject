7db039e65fc58edfb1326d95504461a0d1231c62
Paweł Pradelok <code2pradelok@gmail.com>
2019-06-11 23:33:22 +0200

    dodana metoda find by addmin id ale nie działa

--- src/main/java/pl/coderslab/web/RecipeList.java
+++ src/main/java/pl/coderslab/web/RecipeList.java
@@ -16,9 +16,10 @@ public class RecipeList extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        Admin admin = (Admin) session.getAttribute("admin");
-        int id = admin.getId();
-        req.setAttribute("id", id);
+        Admin admin = (Admin) session.getAttribute("admin"); //zczytany admin z sesji
+        int id = admin.getId(); //zczytane admin id
+
+
 
         getServletContext().getRequestDispatcher("/recipe_list.jsp").forward(req, resp);
     }
