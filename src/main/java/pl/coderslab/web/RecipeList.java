5c266abcf69aa61d44863534abbc89f650e67ffb
Paweł Pradelok <code2pradelok@gmail.com>
2019-06-11 21:55:48 +0200

    recipelist v2

--- src/main/java/pl/coderslab/web/RecipeList.java
+++ src/main/java/pl/coderslab/web/RecipeList.java
@@ -1,10 +1,13 @@
 package pl.coderslab.web;
 
+import pl.coderslab.model.Admin;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(urlPatterns = "/app.recipe/list")
@@ -12,6 +15,11 @@ public class RecipeList extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Admin admin = (Admin) session.getAttribute("admin");
+        int id = admin.getId();
+        req.setAttribute("id", id);
+
         getServletContext().getRequestDispatcher("/recipe_list.jsp").forward(req, resp);
     }
 }
