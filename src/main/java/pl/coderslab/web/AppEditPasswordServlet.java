f1bafdf5c614d1fcf7794719c58d044fc2bee13e
Ula Placek <ulaplacek@gmail.com>
2019-06-12 15:50:20 +0200

    user name at header added

--- src/main/java/pl/coderslab/web/AppEditPasswordServlet.java
+++ src/main/java/pl/coderslab/web/AppEditPasswordServlet.java
@@ -1,10 +1,13 @@
 package pl.coderslab.web;
 
+import pl.coderslab.model.Admin;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet (urlPatterns = "/app.editPassword")
@@ -12,6 +15,8 @@ public class AppEditPasswordServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        req.setAttribute("admin",(Admin)session.getAttribute("admin"));
 
         getServletContext().getRequestDispatcher("/app.editPassword.jsp").forward(req,resp);
     }
