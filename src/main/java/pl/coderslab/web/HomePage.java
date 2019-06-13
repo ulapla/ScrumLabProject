2b494da51cf9a4c07b5a8cda5c0f9ec943609c02
Ula Placek <ulaplacek@gmail.com>
2019-06-13 21:08:28 +0200

    home page address changed - now it is working

--- src/main/java/pl/coderslab/web/HomePage.java
+++ src/main/java/pl/coderslab/web/HomePage.java
@@ -10,18 +10,20 @@ import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.Map;
 
-@WebServlet(urlPatterns = "/")
+@WebServlet(urlPatterns = "/home")
 public class HomePage extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Map<String, String[]> parameterMap = req.getParameterMap();
-        for(String param: parameterMap.keySet()){
-            if(param.equals("logout")){
-                req.getSession().removeAttribute("admin");
-            }
+
+        if(req.getParameter("logout").equals((String)"1")) {
+            System.out.println("działa");
+            System.out.println(req.getParameter("logout"));
+            req.getSession().removeAttribute("admin");
         }
-        req.getSession().removeAttribute("admin");
+
+
+
 
         getServletContext().getRequestDispatcher("/home.jsp").forward(req, resp);
     }
