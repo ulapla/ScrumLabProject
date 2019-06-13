a197a4e913f34d83f989dc9cfb3ad69830532e7e
Ula Placek <ulaplacek@gmail.com>
2019-06-13 20:04:35 +0200

    not working

--- src/main/java/pl/coderslab/web/HomePage.java
+++ src/main/java/pl/coderslab/web/HomePage.java
@@ -6,12 +6,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Map;
 
 @WebServlet(urlPatterns = "/")
 public class HomePage extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        for(String param: parameterMap.keySet()){
+            if(param.equals("logout")){
+                req.getSession().removeAttribute("admin");
+            }
+        }
+        req.getSession().removeAttribute("admin");
+
         getServletContext().getRequestDispatcher("/home.jsp").forward(req, resp);
     }
 }
